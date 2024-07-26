20b5641eaa554dc2a6cd5fc37d4a90f36d64e041
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-07-26 15:38:22 +0200

    updating composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd84dff25c107d049568a47b0aeccc8d64c52e9ae',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd84dff25c107d049568a47b0aeccc8d64c52e9ae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
