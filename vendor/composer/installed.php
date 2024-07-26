3595f9301386e14e3a1b6830793358de927d3bb1
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-07-26 10:27:24 +0200

    testing for deployment
    removed gitignore files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'brieuc/restaurant-2.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9e5867072c5f4df28d4f819648b135fed5872809',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brieuc/restaurant-2.0' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9e5867072c5f4df28d4f819648b135fed5872809',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
